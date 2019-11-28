ff9a03df8c0e6fb984659cdaccbe667861885b19
Sonia Quintiao <soniaquintiao@gmail.com>
2019-11-28 13:45:14 +0000

    Modify '/' route: Passes 'viewing index page' feature test

--- app.rb
+++ app.rb
@@ -6,11 +6,14 @@ class BookmarkManager < Sinatra::Base
 # get '/' do
 #   "Hello world!"
 # end
+get '/' do
+  'Bookmark Manager'
+end
+
 get '/bookmarks' do
   @bookmarks = Bookmark.all
   erb :'bookmarks/index'
 end
-
 
 # start the server if ruby file executed directly
   run! if app_file == $0
