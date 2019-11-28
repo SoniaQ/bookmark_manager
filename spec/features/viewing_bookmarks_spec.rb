d9ab66caf3eb076f49fca8c4b02bd0eecee65f0b
Sonia Quintiao <soniaquintiao@gmail.com>
2019-11-28 13:40:22 +0000

    Update feature to test: Page title on load is equal to 'Bookmark Manager'

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,8 +1,17 @@
 feature 'Viewing bookmarks' do
+
+  scenario 'visiting the index page' do
+    visit '/'
+    expect(page).to have_content "Bookmark Manager"
+  end
+
   scenario 'A user can see bookmarks' do
     visit '/bookmarks'
-    expect(page).to have_content "https://makers.tech/"
-    expect(page).to have_content "http://google.com"
+    # expect(page).to have_content "https://makers.tech/"
+    # expect(page).to have_content "http://google.com"
+    expect(page).to have_content "https://askjeeves.net/"
     expect(page).to have_content "https://snake.googlemaps.com/"
+    expect(page).to have_content "https://beeroclock.in/"
   end
+
 end
